d15d12ee89f3b5332108c02c282fa904f4b6a804
paulkowa <paulkowa@buffalo.edu>
2015-10-05 22:38:16 -0400

    Updated with all runtime output and files types minus rolling median

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ If no output filename is provided the following will be the output files:
 
 bigpyRemoved.fsa - Lists all sequences, with headers, from the input file were filtered during the processing stage.
 
-bigpyClean.txt - Lists all sequences, without headers, kept after filtering. A single line stores one text sequence.
+bigpyClean.txt - Lists all sequences, without headers, kept after filtering. A single line stores one text sequence and its ID.
 
-bigpyMap.fsa - Lists all sequences kept after filtering with a sequence ID. ID is the original header from the input file.
+bigpyMap.fsa - Lists all sequences kept after filtering with a sequence ID. ID is the line in the bigpySeqs.seq and bigpyClean.txt files in which the sequence is listed.
 
-bigpySeqs.seq - Lists all in binary to be used for parallel step. A single line stores one sequence.
+bigpySeqs.seq - Lists all in binary with ID to be used for parallel step. A single line stores one sequence.
--- bigpy-prepare.py
+++ bigpy-prepare.py
@@ -10,31 +10,125 @@ Distributed under the MIT License.
 See accompanying file LICENSE_MIT.txt.
 
 This file is part of BiGPy.
- '''
+'''
 #!/usr/bin/python
 
 from Bio import SeqIO
 from optparse import OptionParser
+from collections import namedtuple
+import timeit
 import fileinput
 import re
 import binascii
-
-#def info(options):
-
+import os
 
 # Handle command line arg options
 def setUp():
     parser = OptionParser()
-    
-    parser.add_option("-i", "--input", dest = "input", help = "FILE to read input sequence list from", metavar = "FILE")
+    parser.add_option("-i", "--input", dest = "input", help = "{FILE|DIR} to read input sequence list from", metavar = "{FILE|DIR}")
     parser.add_option("-o", "--output", dest = "output", default = "bigpy", help = "FILE to print results to", metavar = "FILE")
     parser.add_option("-l", "--length", dest = "length", default = 100, help = "Filter sequences shorter than SIZE", metavar = "SIZE")
     parser.add_option("-d", "--dna", dest = "type", default = True, help = "DNA/RNA or amino acid sequences supported\nTrue = DNA\nFalse = amino acid", metavar = "{True|False}")
     parser.add_option("-c", "--clean", dest = "clean", default = True, help = "Remove sequences that contain invalid characters", metavar = "{True|False}")
-    
     (options, args) = parser.parse_args()
+
     return options
 
+# Run and print all information
+def run(options):
+    fileList = []
+    print "scanning " + options.input + " for input files..."
+    files = checkDir(options, fileList)
+    print "found " + str(files) + " input file(s)\nextracting sequences..."
+    start_time = timeit.default_timer()
+    statStruct = parse(options, fileList)
+    elapsed = timeit.default_timer() - start_time
+    print "valid " + statStruct.validSeqs + " out of " + statStruct.countSeqs + " sequences\nwriting output files...\n" + \
+        "shortest sequence: " + statStruct.shortSeq + "\nlongest sequence: " + statStruct.longSeq + "\naverage sequence: " + statStruct.avgSeq + \
+        "\nmedian sequence: " + statStruct.medSeq + "\ntime: " + str(elapsed) + " seconds\ndone!"
+
+
+# Check input directory for all fasta / fsa files
+def checkDir(options, fileList):
+    inputFiles = 0
+    if options.input[-1:] == '/':
+        for file in os.listdir(options.input):
+            if file.endswith(".fsa") or file.endswith(".fasta"):
+                fileList.append(str(file))
+                inputFiles += 1
+    else:
+        fileList.append(options.input)
+        inputFiles += 1
+    return inputFiles
+
+
+# Parse the input file and print to output file cleaned sequences
+def parse(options, fileList):
+    # Open output files
+    seqFile = open(options.output + "Seqs.seq", 'w+')
+    cleanFile = open(options.output + "Clean.txt", "w+")
+    mapFile = open(options.output + "Map.fsa", "w+")
+    removedSeqs = open(options.output + "Removed.fsa", "w+")
+
+    # Create statistic storage
+    statStruct = namedtuple('statStruct', ['countSeqs', 'validSeqs', 'shortSeq', 'longSeq', 'avgSeq', 'medSeq'])
+    count, valid, shortest, longest, avg, median = (0,) * 6
+
+    # Iterate through input and write to output files
+    for f in fileList:
+        if options.input[-1:] == '/':
+            inFile = open(options.input + f, 'rU')
+
+        else:
+            inFile = open(f, 'rU')
+
+        for record in SeqIO.parse(inFile, "fasta"):
+            # Record longest and shorest sequences
+            if count == 0:
+                shortest = len(record.seq)
+                longest = len(record.seq)
+                avg = len(record.seq)
+            elif len(record.seq) < shortest:
+                shortest = len(record.seq)
+            elif len(record.seq) > longest:
+                longest = len(record.seq)
+            avg = average(len(record.seq), count + 1, avg)
+
+            # Process sequence                
+            if options.clean:
+                if (len(record.seq) > options.length and checkAlphabet(str(record.seq), options)):
+                    seqFile.write(str(bin(count)) + '\t' + text_to_bi(str(record.seq)) + '\n')
+                    cleanFile.write(str(bin(count)) + '\t' + str(record.seq) + '\n')
+                    mapFile.write(str(count) + '\t' + str(record.description) + '\n')
+                    valid += 1
+                else:
+                    removedSeqs.write(str(record.description) + '\n' + str(record.seq) + '\n')
+                
+            else:
+                if (len(record.seq) > options.length):
+                    seqFile.write(str(bin(count)) + '\t' + text_to_bi(str(record.seq)) + '\n')
+                    cleanFile.write(str(bin(count)) + '\t' + str(record.seq) + '\n')
+                    mapFile.write(str(count) + '\t' + str(record.description) + '\n')
+                else:
+                    removedSeqs.write(str(record.description) + '\n' + str(record.seq) + '\n')
+                valid += 1
+            count += 1
+
+        inFile.close()
+
+    seqFile.close()
+    cleanFile.close()
+    mapFile.close()
+    removedSeqs.close()
+
+    return statStruct(str(count), str(valid), str(shortest), str(longest), str(avg), str(median))
+
+# Compute rolling average
+def average(length, count, avg):
+    if count == 1:
+        return avg
+    else:
+        return (avg + ((length - avg) / (count + 1)))
 
 # Checks if sequence contains only valid characters
 def checkAlphabet(seq, options):
@@ -42,24 +136,22 @@ def checkAlphabet(seq, options):
     if options.type:
         if re.match("^[ACTG]*$", seq):
             return True
+
     # Check if valid protein sequence
     else:
         if re.match("^[ACDEFGHIKLMNPQRSTVWY]*$", seq):
             return True
     return False
 
-
 # Convert string to binary
 def text_to_bi(text, encoding='utf-8', errors='surrogatepass'):
     bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-
 # Convert binary to string
 def text_from_bi(bits, encoding='utf-8', errors='surrogatepass'):
     n = int(bits, 2)
     return int2bytes(n).decode(encoding, errors)
-
 
 # Break binary sequence into bytes for conversion to ACSII
 def int2bytes(i):
@@ -67,48 +159,9 @@ def int2bytes(i):
     n = len(hex_string)
     return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
 
-
-# Parse the input file and print to output file cleaned sequences
-def parse(options):
-    inFile = open(options.input, 'rU')
-    seqFile = open(options.output + "Seqs.seq", 'w+')
-    cleanFile = open(options.output + "Clean.txt", "w+")
-    mapFile = open(options.output + "Map.fsa", "w+")
-    removedSeqs = open(options.output + "Removed.fsa", "w+")
-    n = 0;
-
-    # Iterate through input and write to output files
-    for record in SeqIO.parse(inFile, "fasta"):
-        if options.clean:
-            if (len(record.seq) > options.length and checkAlphabet(str(record.seq), options)):
-                seqFile.write(str(bin(n)) + '\t' + text_to_bi(str(record.seq)) + '\n')
-                cleanFile.write(str(bin(n)) + '\t' + str(record.seq) + '\n')
-                mapFile.write(str(n) + '\t' + str(record.description) + '\n')
-            else:
-                removedSeqs.write(str(record.description) + '\n' + str(record.seq) + '\n')
-                
-        else:
-            if (len(record.seq) > options.length):
-                seqFile.write(str(bin(n)) + '\t' + text_to_bi(str(record.seq)) + '\n')
-                cleanFile.write(str(bin(n)) + '\t' + str(record.seq) + '\n')
-                mapFile.write(str(n) + '\t' + str(record.description) + '\n')
-            else:
-                removedSeqs.write(str(record.description) + '\n' + str(record.seq) + '\n')
-        n += 1
-
-    inFile.close()
-    seqFile.close()
-    cleanFile.close()
-    mapFile.close()
-    removedSeqs.close()
-
-
 # Main
 def main():
-
-    options = setUp()
-    #info(options)
-    parse(options)
+    run(setUp())
 
 if __name__ == "__main__":
     main()
